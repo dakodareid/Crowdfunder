66ae0f0aba4ae241809ba85c87a08a94103ac76d
Amy Rudolph <amy.rudolph@live.co.uk>
2014-12-03 16:11:10 -0500

    added alert to signed up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   	@user = User.new(user_params)
   	if @user.save
   		auto_login(@user)
-  		redirect_to projects_url
+  		redirect_to projects_url, notice: "Login Successful!"
   	else
   		render 'new'
   	end
